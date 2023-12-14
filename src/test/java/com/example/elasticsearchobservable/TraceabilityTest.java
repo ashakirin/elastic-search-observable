913b51e88817b4d2b1111e88e2554ba1c6b7354b
Andrei Shakirin <ashakirin@vmware.com>
2023-12-14 09:53:02 +0100

    Fixed test

--- src/test/java/com/example/elasticsearchobservable/TraceabilityTest.java
+++ src/test/java/com/example/elasticsearchobservable/TraceabilityTest.java
@@ -2,13 +2,17 @@ package com.example.elasticsearchobservable;
 
 import com.example.elasticsearchobservable.elastic.Article;
 import com.example.elasticsearchobservable.elastic.ArticleRepository;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.observability.DefaultSignalListener;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @SpringBootTest
 //@ExtendWith(OutputCaptureExtension.class)
@@ -17,11 +21,33 @@ public class TraceabilityTest {
     @Autowired
     private ArticleRepository repository;
 
+    @Autowired
+    private ObservationRegistry observationRegistry;
+
     @Test
     public void checkTracingInLog() {
-        MDC.put("traceId", "my trace id");
-        MDC.put("spanId", "my span id");
-        logger.info("before");
+    // Create an Observation and observe your code!
+        Observation.createNotStarted("user.name", observationRegistry)
+                .contextualName("getting-user-name")
+                .observe(() -> {
+                    logger.info("Hello");
+                    fluxIteration();
+                    elasticSearchRepoCall();
+                });
+
+//        MDC.put("traceId", "my trace id");
+//        MDC.put("spanId", "my span id");
+    }
+
+    private static void fluxIteration() {
+        Flux.just(1, 2, 3)
+                .delayElements(Duration.ofMillis(1))
+                        .handle((result, sink) -> logger.info("inside flux handle"))
+                .contextCapture()
+                .blockLast();
+    }
+
+    private void elasticSearchRepoCall() {
         repository.findByName("Article 1")
                 .map(Article::getName)
                 .tap(() -> new DefaultSignalListener<>() {
@@ -33,9 +59,11 @@ public class TraceabilityTest {
                 })
                 .handle((result, sink) -> {
                     logger.info("inside handle");
-  //                  assertThat(output.getOut()).contains("my trace id");
+                    //                  assertThat(output.getOut()).contains("my trace id");
                     sink.next(result);
                 })
+                .contextCapture()
                 .block();
     }
+
 }
